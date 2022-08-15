1357882d4876a5a9b1012d39bbdc0cf57093656d
Regan-John Daniels <reganjohndaniels@gmail.com>
2022-08-15 22:21:06 +0200

    add functionality
    Added functionality to automatically update dimension tables with new data that comes in

--- upload_transactions.py
+++ upload_transactions.py
@@ -8,7 +8,32 @@ def upload_transactions(data_file_dir:str):
     categories = hf.get_data('select * from categories_DIM', 'budget')
     spending_groups = hf.get_data('select * from spending_groups_DIM', 'budget')
     accounts = hf.get_data('select * from accounts_DIM', 'budget')
-    dates = hf.get_data('select * from Date_DIM', 'budget')
+
+    ### update dimension tables
+    dimension_data = [categories, spending_groups, accounts]
+    dimension_update_variables = {'categories':{'trxs_column_name': 'Category',
+                                                'db_column_name': 'category_name',
+                                                'db_tbl_name': 'categories_DIM'},
+                                'spending_groups': {'trxs_column_name': 'Spending Group',
+                                            'db_column_name': 'spending_group_name',
+                                            'db_tbl_name': 'spending_groups_DIM'},
+                                'accounts': {'trxs_column_name': 'Account',
+                                            'db_column_name': 'account_name',
+                                            'db_tbl_name': 'accounts_DIM'}}
+
+    for i, v in enumerate(dimension_data):
+        trxs_values = list(transactions[list(dimension_update_variables.values())[i].get('trxs_column_name')].unique())
+        db_values = list(v[list(dimension_update_variables.values())[i].get('db_column_name')])
+        values_to_add = [x for x in trxs_values if x not in db_values]
+        
+        if len(values_to_add > 0):
+            for j, k in enumerate(values_to_add):
+                query = f'''
+                    INSERT INTO [dbo].[{list(dimension_update_variables.values())[j].get('db_tbl_name')}] ([{list(dimension_update_variables.values())[j].get('db_column_name')}]) VALUES ('{k}');
+                    '''
+                print(query)
+        # cur.execute(query)
+
 
     spending_group_mapping = dict(spending_groups.iloc[:, [1, 0]].values)
     category_mapping = dict(categories.iloc[:, [1, 0]].values)
